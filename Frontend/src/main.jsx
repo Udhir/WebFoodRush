b26f8b089fd67d9e34cdf2b9dcdb1b8e97e2a3e4
Udhir <messiudhir10@gmail.com>
2026-07-15 19:02:08 +0545

    Updating Frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import {Toaster} from "react-hot-toast";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
+
     <BrowserRouter>
 
         <AuthProvider>
+
+        <Toaster/>
 
             <App />
 
